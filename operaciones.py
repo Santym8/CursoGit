3fd7c60813dcde1a7b8eaec083f1841df43520eb
Santy <santimarcelofierrod@gmail.com>
2021-01-13 17:07:07 -0500

    Funcion potencia y llamada a las funciones

--- operaciones.py
+++ operaciones.py
@@ -1,11 +1,13 @@
 
 def suma(n1, n2):
-    return n1 + n2
+    return str(n1 + n2)
 
 def resta(n1, n2):
-    return n1 - n2
+    return str(n1 - n2)
 
+def potencia(n1, ex):
+    return str(n1**ex)
 
-
-    
-
+print(suma(2,3))
+print(resta(5,1))
+print(potencia(2,3))
