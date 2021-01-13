19acc3632325c9e19620f4afb5b0afadf854e1d3
Santy <santimarcelofierrod@gmail.com>
2021-01-13 17:10:59 -0500

    Funcion multiplicacion y division, llamada a las funciones

--- operaciones.py
+++ operaciones.py
@@ -5,7 +5,14 @@ def suma(n1, n2):
 def resta(n1, n2):
     return n1 - n2
 
+def multiplicacion(n1, n2):
+    return n1 * n2
+
+def division(n1, n2):
+    return n1/n2
 
 
+print(multiplicacion(5,5))
+print(division(9,3))
     
 
